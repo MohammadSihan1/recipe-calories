77a4a4123aef63a08ab29c28422087c56d7ceec1
MohammadSihan1 <mohammadsihan289@gmail.com>
2024-08-09 19:41:10 +0600

    banner added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div>
+            <header className="banner text-stone-500 text-right p-32 rounded-3xl my-7 shadow-2xl">
+                <h2 className="text-7xl my-6">Discover an<br />  exceptional cooking class <br />  tailored for you!</h2>
+                <p className="text-2xl">Learn and Master Basic Programming, <br /> Data Structures, Algorithm, OOP, Database and solve 500+ <br />  coding problems to become an exceptionally well world-class Programmer.</p>
+            </header>
+        </div>
+    );
+};
+
+export default Banner;
